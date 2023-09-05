dd619c87e9fae340d42ede2ab3452954a356faf8
sandeep kumar uppari <upparisandeep77@gmail.com>
2023-09-05 22:11:19 +0530

    added

---
+++ twentyone-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the heading created by sandeep</h1>
+      <h1>This is the heading created by sravan</h1>
+    </div>
+  );
+}
+
+export default App;
